7c1752f7cbfda205dc7494ec04f5a0d240fbaddb
luke-lemon <luke@lemoncloud.io>
2025-04-10 15:48:57 +0900

    feat: rename DashBoardPage to HomePage

--- apps/web/src/app/features/home/pages/DashboardPage.tsx
+++
@@ -1,3 +0,0 @@
-export const DashboardPage = () => {
-    return <div>TODO: add page</div>;
-};
---
+++ apps/web/src/app/features/home/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+export const HomePage = () => {
+    return (
+        <div className="h-full flex flex-col">
+            <div className="bg-red-300 flex-1">Hello</div>
+            <div className="bg-red-00 flex-1">Hello</div>
+        </div>
+    );
+};
