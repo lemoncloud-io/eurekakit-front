97742b83d9f3bd5e4e7bce022343c766dd851f30
luke-lemon <luke@lemoncloud.io>
2025-04-10 15:51:30 +0900

    feat: simplify TabButton component by consolidating className logic

--- apps/web/src/app/components/tab-bar/TabBar.tsx
+++ apps/web/src/app/components/tab-bar/TabBar.tsx
@@ -5,34 +5,19 @@ import { TabButton } from './TabBarButton';
 export const TabBar = () => {
     return (
         <nav className="sticky h-16 w-full bg-background bottom-0 border-t flex justify-around items-center">
-            <TabButton
-                to="/home"
-                className={isActive => (isActive.isActive ? 'text-foreground' : 'text-muted-foreground')}
-            >
+            <TabButton to="/home">
                 <Home size={24} />
             </TabButton>
-            <TabButton
-                to="/search"
-                className={isActive => (isActive.isActive ? 'text-foreground' : 'text-muted-foreground')}
-            >
+            <TabButton to="/search">
                 <Search size={24} />
             </TabButton>
-            <TabButton
-                to="/post/create"
-                className={isActive => (isActive.isActive ? 'text-foreground' : 'text-muted-foreground')}
-            >
+            <TabButton to="/post/create">
                 <PlusSquare size={24} />
             </TabButton>
-            <TabButton
-                to="/feed"
-                className={isActive => (isActive.isActive ? 'text-foreground' : 'text-muted-foreground')}
-            >
+            <TabButton to="/feed">
                 <LayoutIcon size={24} />
             </TabButton>
-            <TabButton
-                to="/user"
-                className={isActive => (isActive.isActive ? 'text-foreground' : 'text-muted-foreground')}
-            >
+            <TabButton to="/user">
                 <User size={24} />
             </TabButton>
         </nav>
--- apps/web/src/app/components/tab-bar/TabBarButton.tsx
+++ apps/web/src/app/components/tab-bar/TabBarButton.tsx
@@ -1,10 +1,20 @@
 import { NavLink } from 'react-router-dom';
 
+import { cn } from '@lemon/ui-kit';
+
 import type { NavLinkProps } from 'react-router-dom';
 
 export const TabButton = ({ className, children, ...props }: NavLinkProps) => {
     return (
-        <NavLink className={params => (typeof className === 'function' ? className(params) : className)} {...props}>
+        <NavLink
+            className={params =>
+                cn(
+                    params.isActive ? 'text-foreground' : 'text-muted-foreground',
+                    typeof className === 'function' ? className(params) : className
+                )
+            }
+            {...props}
+        >
             {children}
         </NavLink>
     );
