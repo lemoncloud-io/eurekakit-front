7caa9c8f92b102395e26c8816a4663144057a8f9
louis <louis@lemoncloud.io>
2025-04-11 13:37:07 +0900

    refactor: rename mobile to EurekaPage

--- apps/mobile/android/app/src/main/java/io/lemoncloud/eurekapage/MainApplication.kt
+++ apps/mobile/android/app/src/main/java/io/lemoncloud/eurekapage/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.mobile
+package io.lemoncloud.eurekapage
 
 import android.app.Application
 import com.facebook.react.PackageList
--- apps/mobile/src/main.tsx
+++ apps/mobile/src/main.tsx
@@ -2,4 +2,4 @@ import { AppRegistry } from 'react-native';
 
 import App from './app/App';
 
-AppRegistry.registerComponent('Mobile', () => App);
+AppRegistry.registerComponent('EurekaPage', () => App);
