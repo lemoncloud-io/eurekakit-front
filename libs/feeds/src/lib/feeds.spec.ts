85f6a6208a49f075949239e88994719a052e489b
luke-lemon <luke@lemoncloud.io>
2025-04-11 14:47:32 +0900

    feat: add feeds lib

---
+++ libs/feeds/src/lib/feeds.spec.ts
@@ -0,0 +1,7 @@
+import { feeds } from './feeds';
+
+describe('feeds', () => {
+    it('should work', () => {
+        expect(feeds()).toEqual('feeds');
+    });
+});
---
+++ libs/feeds/src/lib/feeds.ts
@@ -0,0 +1,3 @@
+export function feeds(): string {
+    return 'feeds';
+}
