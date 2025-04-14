ca0a6f6361584efe100ecca282aab5866e21eeb9
luke-lemon <luke@lemoncloud.io>
2025-04-14 14:31:54 +0900

    feat: add Client FeedView type

--- libs/feeds/src/types/index.ts
+++ libs/feeds/src/types/index.ts
@@ -1,6 +1,11 @@
-import type { FeedListParam } from '@lemoncloud/pets-socials-api';
+import type { RequiredKeys } from '@lemon/shared';
+import type { FeedListParam as OFeedListParam, FeedView as OFeedView, UserView } from '@lemoncloud/pets-socials-api';
 
-export interface FeedListParams extends FeedListParam {
+export interface FeedListParam extends OFeedListParam {
     activity?: boolean;
     parent?: boolean;
+}
+
+export interface FeedView extends Omit<RequiredKeys<OFeedView, 'id' | 'name' | 'createdAt'>, 'user$'> {
+    user$: RequiredKeys<UserView, 'id' | 'nick'>;
 }
