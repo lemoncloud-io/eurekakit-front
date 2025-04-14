58de9975ce9d0119d4c2236d2282b25b504f8769
luke-lemon <luke@lemoncloud.io>
2025-04-14 13:03:39 +0900

    fix: fix response type of fetchFeedList

--- libs/feeds/src/features/list/apis/fetchFeedList.ts
+++ libs/feeds/src/features/list/apis/fetchFeedList.ts
@@ -3,13 +3,14 @@ import { webCore } from '@lemon/web-core';
 import { BACKEND_API, FEEDS, LIST_V2 } from '../../../consts';
 
 import type { FeedListParams } from '../../../types';
+import type { ListResult } from '@lemoncloud/codes-backend-api/dist/cores/types';
 import type { FeedView } from '@lemoncloud/pets-socials-api';
 
 export const fetchFeedList = async (params?: FeedListParams) => {
     const { data } = await webCore
         .buildSignedRequest({ method: 'GET', baseURL: [BACKEND_API, FEEDS, 0, LIST_V2].join('/') })
         .setParams({ ...params })
-        .execute<FeedView>();
+        .execute<ListResult<FeedView>>();
 
     return data;
 };
