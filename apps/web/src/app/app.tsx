358ba959973e292281fca0dd6f2034e661c970e5
luke-lemon <luke@lemoncloud.io>
2025-04-14 15:34:45 +0900

    feat: set throwOnError when query error

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -18,6 +18,7 @@ export function App() {
         defaultOptions: {
             queries: {
                 staleTime: Infinity,
+                throwOnError: true,
             },
         },
     });
