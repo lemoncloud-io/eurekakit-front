37f53c6e0199d2ae58c6f611b5d1213637b3b324
luke-lemon <luke@lemoncloud.io>
2025-04-14 14:07:29 +0900

    fix: add hasNextPage condition to button rendering in TotalPostList

--- apps/web/src/app/features/home/components/total-post-list/TotalPostList.tsx
+++ apps/web/src/app/features/home/components/total-post-list/TotalPostList.tsx
@@ -6,21 +6,23 @@ import { NoPost } from '../no-post';
 import { PostList } from '../post-list/PostList';
 
 export const TotalPostList = () => {
-    const { data: feedList, fetchNextPage, isFetching } = useFetchInfiniteFeedList({ limit: 5 });
+    const { data: feedList, fetchNextPage, isFetching, hasNextPage } = useFetchInfiniteFeedList({ limit: 5 });
 
     return (
         <div className="flex flex-col p-4">
             <h3 className="font-semibold">전체글</h3>
             <Condition condition={!!feedList?.total} fallback={<NoPost />}>
                 <PostList postList={feedList?.list} />
-                <Button
-                    className="w-full gap-2"
-                    variant={'secondary'}
-                    onClick={() => fetchNextPage()}
-                    isLoading={isFetching}
-                >
-                    더보기
-                </Button>
+                <Condition condition={hasNextPage}>
+                    <Button
+                        className="w-full gap-2"
+                        variant={'secondary'}
+                        onClick={() => fetchNextPage()}
+                        isLoading={isFetching}
+                    >
+                        더보기
+                    </Button>
+                </Condition>
             </Condition>
         </div>
     );
