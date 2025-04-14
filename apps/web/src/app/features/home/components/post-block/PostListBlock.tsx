4951c57a2fda2e48bec0bc6fcf50a6ff2d2a37b8
luke-lemon <luke@lemoncloud.io>
2025-04-14 17:14:31 +0900

    fix: fix typo

--- apps/web/src/app/features/home/components/post-block/PostListBlock.tsx
+++ apps/web/src/app/features/home/components/post-block/PostListBlock.tsx
@@ -19,7 +19,7 @@ export const PostListBlock = ({ post, onClickLike }: PostListBlockProps) => {
             <div className="flex w-full flex-col justify-between gap-1">
                 <div className="flex flex-col gap-1">
                     <p className="line-clamp-2 break-all">{post.name}</p>
-                    <List hotizontal seperator={<span>·</span>} className="text-muted-foreground gap-1 text-sm">
+                    <List horizontal seperator={<span>·</span>} className="text-muted-foreground gap-1 text-sm">
                         <span className="line-clamp-1">{post.user$.nick}</span>
                         <span>{formatRelativeTime(post.createdAt ?? 0)}</span>
                     </List>
