5d9d14f2b7fa8e5ddc644912b72af6bf2c848313
luke-lemon <luke@lemoncloud.io>
2025-04-14 15:35:51 +0900

    feat: add mock like feed api

--- libs/feeds/src/mocks/feedHandlers.ts
+++ libs/feeds/src/mocks/feedHandlers.ts
@@ -1,10 +1,12 @@
 import { HttpResponse, delay, http } from 'msw';
 
-import { BACKEND_API, FEEDS, LIST_V2 } from '../consts';
+import { ACTIVITY, BACKEND_API, FEEDS, LIST_V2 } from '../consts';
 import { feedList } from './mockFeeds';
 
 import type { FeedView } from '../types';
 import type { ListResult } from '@lemoncloud/codes-backend-api/dist/cores/types';
+
+let mutableFeedList = [...feedList];
 
 export const feedsHandler = [
     http.get([BACKEND_API, FEEDS, 0, LIST_V2].join('/'), async ({ request }) => {
@@ -12,17 +14,34 @@ export const feedsHandler = [
         const limit = Number(new URL(request.url).searchParams.get('limit')) || 10;
 
         const startFeedIdx = page * limit;
-        const endFeedIdx = Math.min((page + 1) * limit, feedList.length);
+        const endFeedIdx = Math.min((page + 1) * limit, mutableFeedList.length);
 
-        const responseFeedList = feedList.slice(startFeedIdx, endFeedIdx);
+        const responseFeedList = mutableFeedList.slice(startFeedIdx, endFeedIdx);
 
         await delay(1000);
 
         return HttpResponse.json({
             list: responseFeedList,
-            total: feedList.length,
+            total: mutableFeedList.length,
             page,
             limit,
         } as ListResult<FeedView>);
+    }),
+    http.put([BACKEND_API, FEEDS, ':id', ACTIVITY].join('/'), async ({ request, params }) => {
+        const feedId = params['id'];
+
+        const toBeLike = JSON.parse(new URL(request.url).searchParams.get('like')!) as boolean;
+
+        const targetFeed = mutableFeedList.find(feed => feed.id === feedId)!;
+        const toBeFeed = {
+            ...targetFeed,
+            $activity: { ...targetFeed?.$activity, isLike: toBeLike },
+        };
+
+        mutableFeedList = mutableFeedList.map(feed => (feed.id !== targetFeed.id ? feed : toBeFeed));
+
+        await delay(1000);
+
+        return HttpResponse.json(toBeFeed);
     }),
 ];
