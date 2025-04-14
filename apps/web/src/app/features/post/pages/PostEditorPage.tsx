eb0ef74acd8435efa9be2db5fc4b79837371bd33
luke-lemon <luke@lemoncloud.io>
2025-04-14 17:51:09 +0900

    feat: add react-hook-form in create post page

--- apps/web/src/app/features/post/pages/PostEditorPage.tsx
+++ apps/web/src/app/features/post/pages/PostEditorPage.tsx
@@ -1,11 +1,21 @@
+import { useForm } from 'react-hook-form';
+
+import { DevTool } from '@hookform/devtools';
 import { Image, X } from 'lucide-react';
 
 import { Button } from '@lemon/ui-kit/components/ui/button';
+import { Form, FormControl, FormField, FormItem, FormLabel } from '@lemon/ui-kit/components/ui/form';
 import List from '@lemon/ui-kit/components/ui/list';
 import { Separator } from '@lemon/ui-kit/components/ui/separator';
 import { Textarea } from '@lemon/ui-kit/components/ui/textarea';
 
+import { isDev } from '../../../utils';
+
+import type { FeedBody } from '@lemoncloud/pets-socials-api';
+
 export const PostEditorPage = () => {
+    const methods = useForm<FeedBody>({});
+
     return (
         <div className="w-full">
             <header className="flex h-12 w-full items-center justify-between border-b px-2">
@@ -16,32 +26,46 @@ export const PostEditorPage = () => {
                 </Button>
             </header>
             <div className="flex w-full flex-col gap-3 p-4">
-                <span>
-                    내용
-                    <span className="text-destructive ml-0.5">*</span>
-                </span>
-                <div className="focus-within:ring-secondary-foreground rounded-lg border focus-within:ring-1">
-                    <Textarea
-                        className="h-72 resize-none rounded-b-none border-none focus-visible:ring-0"
-                        placeholder="내용을 입력해주세요"
+                <Form {...methods}>
+                    {isDev() && <DevTool control={methods.control} />}
+                    <FormField
+                        control={methods.control}
+                        name="text"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>
+                                    내용
+                                    <span className="text-destructive ml-0.5">*</span>
+                                </FormLabel>
+                                <div className="focus-within:ring-secondary-foreground rounded-lg border focus-within:ring-1">
+                                    <FormControl>
+                                        <Textarea
+                                            className="h-72 resize-none rounded-b-none border-none focus-visible:ring-0"
+                                            placeholder="내용을 입력해주세요"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <div className="flex justify-between px-3 py-4 text-xs">
+                                        <span className="text-secondary-foreground">20글자 이상 작성해주세요</span>
+                                        <span className="text-muted-foreground">0/1,500</span>
+                                    </div>
+                                </div>
+                            </FormItem>
+                        )}
                     />
-                    <div className="flex justify-between px-3 py-4 text-xs">
-                        <span className="text-secondary-foreground">20글자 이상 작성해주세요</span>
-                        <span className="text-muted-foreground">0/1,500</span>
-                    </div>
-                </div>
-                <List horizontal>
-                    <Button
-                        variant={'outline'}
-                        size={'icon'}
-                        className="h-[72px] w-[72px] flex-col gap-1 border-dashed"
-                    >
-                        <Image size={24} className="text-secondary-foreground !h-7 !w-7" />
-                        <span className="text-muted-foreground text-xs">0/5</span>
-                    </Button>
-                </List>
-                <Separator className="h-6 bg-transparent" />
-                <Button className="w-full">작성 완료</Button>
+                    <List horizontal>
+                        <Button
+                            variant={'outline'}
+                            size={'icon'}
+                            className="h-[72px] w-[72px] flex-col gap-1 border-dashed"
+                        >
+                            <Image size={24} className="text-secondary-foreground !h-7 !w-7" />
+                            <span className="text-muted-foreground text-xs">0/5</span>
+                        </Button>
+                    </List>
+                    <Separator className="h-6 bg-transparent" />
+                    <Button className="w-full">작성 완료</Button>
+                </Form>
             </div>
         </div>
     );
