beb31e0fea4077e245b00d69c374ab8a3106c021
luke-lemon <luke@lemoncloud.io>
2025-04-16 17:52:18 +0900

    feat: update feed comment handler to support pagination and delay response

--- libs/feeds/src/mocks/feedCommentHandlers.ts
+++ libs/feeds/src/mocks/feedCommentHandlers.ts
@@ -1,4 +1,4 @@
-import { HttpResponse, http } from 'msw';
+import { HttpResponse, delay, http } from 'msw';
 
 import { BACKEND_API, COMMENTS, FEEDS } from '../consts';
 import { commentList } from './mockFeedComments';
@@ -6,20 +6,29 @@ import { commentList } from './mockFeedComments';
 import type { FeedView } from '../types';
 import type { ListResult } from '@lemon/shared';
 
+const mutableFeedCommentList = [...commentList];
+
 export const feedCommentHandler = [
-    http.get([BACKEND_API, FEEDS, ':feedId', COMMENTS].join('/'), ({ request, params }) => {
-        const page = new URLSearchParams(request.url).get('page') || 0;
-        const limit = new URLSearchParams(request.url).get('limit') || 10;
+    http.get([BACKEND_API, FEEDS, ':feedId', COMMENTS].join('/'), async ({ request, params }) => {
+        const page = Number(new URL(request.url).searchParams.get('page')) || 0;
+        const limit = Number(new URL(request.url).searchParams.get('limit')) || 10;
+
+        const startFeedIdx = page * limit;
+        const endFeedIdx = Math.min((page + 1) * limit, mutableFeedCommentList.length);
 
         const parentId = params['feedId'];
-        const targetFeedCommentList = commentList.filter(comment => comment.parentId === parentId);
+        const totalCommentList = mutableFeedCommentList.filter(comment => comment.parentId === parentId);
+
+        const responseCommentList = totalCommentList.slice(startFeedIdx, endFeedIdx);
 
         const response = {
-            list: targetFeedCommentList,
-            total: targetFeedCommentList.length,
+            list: responseCommentList,
+            total: totalCommentList.length,
             page,
             limit,
         } as ListResult<FeedView>;
+
+        await delay(2000);
 
         return HttpResponse.json(response);
     }),
