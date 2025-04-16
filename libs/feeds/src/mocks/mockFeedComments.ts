3191860927227e043a86a69bcf2ae5474ff3e9a7
luke-lemon <luke@lemoncloud.io>
2025-04-16 17:51:27 +0900

    fix: sort mocked comment order by createdAt

--- libs/feeds/src/mocks/mockFeedComments.ts
+++ libs/feeds/src/mocks/mockFeedComments.ts
@@ -1,3 +1,6 @@
 import { feedList, generateFeeds } from './mockFeeds';
 
-export const commentList = feedList.map(feed => generateFeeds(feed.childNo ?? 0, feed.id)).flat();
+export const commentList = feedList
+    .map(feed => generateFeeds(feed.childNo ?? 0, feed.id))
+    .flat()
+    .sort((a, b) => Number(b.createdAt) - Number(a.createdAt));
