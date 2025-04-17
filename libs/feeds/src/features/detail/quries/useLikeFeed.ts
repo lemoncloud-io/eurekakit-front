de0ccc0154fb79da5e216a318b1c497653173161
luke-lemon <luke@lemoncloud.io>
2025-04-17 13:54:14 +0900

    chore: add comment

--- libs/feeds/src/features/detail/quries/useLikeFeed.ts
+++ libs/feeds/src/features/detail/quries/useLikeFeed.ts
@@ -2,6 +2,7 @@ import { useMutation } from '@tanstack/react-query';
 
 import { likeFeed } from '../apis';
 
+// TODO : @luke-lemon 낙관적 업데이트 구현
 export const useLikeFeed = () => {
     return useMutation({
         mutationFn: ({ id, like }: { id?: string; like?: boolean }) => likeFeed(id, like),
