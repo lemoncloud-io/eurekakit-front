fb0f4d5cadb02f98aea7abe558e56380643564e0
luke-lemon <luke@lemoncloud.io>
2025-04-17 16:13:33 +0900

    feat: add dropdownMenu in PostHeader

--- apps/web/src/app/features/post/components/post/PostHeader.tsx
+++ apps/web/src/app/features/post/components/post/PostHeader.tsx
@@ -1,5 +1,12 @@
 import { MoreVerticalIcon } from 'lucide-react';
 
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from '@lemon/ui-kit/components/ui/dropdown-menu';
 import { List } from '@lemon/ui-kit/components/ui/list';
 
 import { NickName, Profile } from '../../../../components';
@@ -9,9 +16,10 @@ interface PostHeaderProps {
     profileImg?: string;
     nickname: string;
     createdAt: EpochTimeStamp;
+    isMe?: boolean;
 }
 
-export const PostHeader = ({ profileImg, nickname, createdAt }: PostHeaderProps) => {
+export const PostHeader = ({ profileImg, nickname, createdAt, isMe }: PostHeaderProps) => {
     return (
         <div className="flex w-full items-center gap-2 py-2">
             <Profile src={profileImg} />
@@ -23,9 +31,22 @@ export const PostHeader = ({ profileImg, nickname, createdAt }: PostHeaderProps)
                 <NickName nickname={nickname} />
                 <span className="text-muted-foreground text-sm">{formatRelativeTime(createdAt)}</span>
             </List>
-            <button className="text-muted-foreground ml-auto aspect-square">
-                <MoreVerticalIcon size={16} />
-            </button>
+            <DropdownMenu>
+                <DropdownMenuTrigger className="text-muted-foreground ml-auto aspect-square">
+                    <MoreVerticalIcon size={16} />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    {isMe ? (
+                        <>
+                            <DropdownMenuItem>수정하기</DropdownMenuItem>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>삭제하기</DropdownMenuItem>
+                        </>
+                    ) : (
+                        <DropdownMenuItem>신고</DropdownMenuItem>
+                    )}
+                </DropdownMenuContent>
+            </DropdownMenu>
         </div>
     );
 };
