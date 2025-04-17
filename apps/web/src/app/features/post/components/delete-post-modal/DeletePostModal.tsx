fce5a9661a31d224e8ac3f3ba79b2cff245d9bc1
luke-lemon <luke@lemoncloud.io>
2025-04-17 17:41:06 +0900

    feat: add DeletePostModal

---
+++ apps/web/src/app/features/post/components/delete-post-modal/DeletePostModal.tsx
@@ -0,0 +1,65 @@
+import { useQueryClient } from '@tanstack/react-query';
+
+import { useDeleteFeed } from '@lemon/feeds';
+import { useGlobalLoader } from '@lemon/shared';
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogTitle,
+} from '@lemon/ui-kit/components/ui/dialog';
+import { Separator } from '@lemon/ui-kit/components/ui/separator';
+
+import { useModalWithDropDown, useNavigate } from '../../../../hooks';
+
+import type { OverlayProps } from '@lemon/overlay';
+
+interface DeletePostModalProps extends OverlayProps {
+    postId: string;
+}
+
+export const DeletePostModal = ({ postId, open, onOpenChange }: DeletePostModalProps) => {
+    const queryClient = useQueryClient();
+    const navigate = useNavigate();
+
+    const { setIsLoading } = useGlobalLoader();
+    const { mutate: deletePost } = useDeleteFeed();
+
+    const modal = useModalWithDropDown(open);
+
+    const onClickDelete = () => {
+        setIsLoading(true);
+        deletePost(postId, {
+            onSuccess: onSuccessDelete,
+        });
+    };
+
+    return (
+        <Dialog modal={modal} open={open} onOpenChange={onOpenChange}>
+            <DialogContent className="gap-4 text-center">
+                <DialogTitle className="flex h-12 items-end justify-center pb-0">
+                    해당 글을 삭제하시겠습니까?
+                </DialogTitle>
+                <DialogDescription className="flex flex-col">
+                    <span>삭제 후 내용은 복원이 불가능하며,</span>
+                    <span>답글 내용도 삭제됩니다.</span>
+                </DialogDescription>
+                <DialogFooter>
+                    <DialogClose className="text-muted-foreground">취소</DialogClose>
+                    <Separator orientation="vertical" />
+                    <DialogClose className="font-semibold" onClick={onClickDelete}>
+                        삭제
+                    </DialogClose>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    );
+
+    async function onSuccessDelete() {
+        setIsLoading(false);
+        navigate(-1);
+        await queryClient.invalidateQueries();
+    }
+};
--- apps/web/src/app/features/post/components/index.ts
+++ apps/web/src/app/features/post/components/index.ts
@@ -1,3 +1,4 @@
 export * from './comment-list';
+export * from './delete-post-modal';
 export * from './post-editor';
 export * from './post';
