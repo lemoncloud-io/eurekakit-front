75343a67e4fbf5f7ad74749867d6878511948459
luke-lemon <luke@lemoncloud.io>
2025-04-18 14:03:52 +0900

    fix: fix type import path

--- libs/feeds/src/features/detail/apis/createFeed.ts
+++ libs/feeds/src/features/detail/apis/createFeed.ts
@@ -2,7 +2,8 @@ import { webCore } from '@lemon/web-core';
 
 import { BACKEND_API, FEED, USERS } from '../../../consts';
 
-import type { FeedBody, FeedView } from '@lemoncloud/pets-socials-api';
+import type { FeedView } from '../../../types';
+import type { FeedBody } from '@lemoncloud/pets-socials-api';
 
 export const createFeed = async (body: FeedBody) => {
     const { data } = await webCore
