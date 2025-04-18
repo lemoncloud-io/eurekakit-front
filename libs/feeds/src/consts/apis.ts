911c93ff56baddec2e57536226c8258522bae28a
luke-lemon <luke@lemoncloud.io>
2025-04-18 17:10:39 +0900

    feat: add create comment api and query

--- libs/feeds/src/consts/apis.ts
+++ libs/feeds/src/consts/apis.ts
@@ -1,10 +1,10 @@
 export const BACKEND_API = import.meta.env.VITE_PET_API_ENDPINT?.toLowerCase();
 
-export const FEED = 'feed';
-export const USERS = 'users';
-
 export const FEEDS = 'feeds';
 export const LIST_V2 = 'list-v2';
 export const DETAIL = 'detail';
 export const ACTIVITY = 'activity';
 export const COMMENTS = 'childs';
+
+export const FEED = 'feed';
+export const USERS = 'users';
--- libs/feeds/src/features/detail/quries/index.ts
+++ libs/feeds/src/features/detail/quries/index.ts
@@ -1,6 +1,8 @@
+export * from './useFetchFeed';
+
 export * from './useCreateFeed';
 export * from './useUpdateFeed';
 export * from './useDeleteFeed';
-
-export * from './useFetchFeed';
 export * from './useLikeFeed';
+
+export * from './useCreateComment';
