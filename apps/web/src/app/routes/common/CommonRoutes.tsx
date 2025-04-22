5fb1f91ae36786cdb3feef32b7ca7e8710effaeb
luke-lemon <luke@lemoncloud.io>
2025-04-22 14:03:50 +0900

    feat: add auth/logout route in common route

--- apps/web/src/app/routes/common/CommonRoutes.tsx
+++ apps/web/src/app/routes/common/CommonRoutes.tsx
@@ -1,12 +1,14 @@
 import { Outlet, type RouteObject } from 'react-router-dom';
 
 import { NotFound } from '../../components';
+import { LogoutPage } from '../../features/auth/pages';
 
 const CommonRoute = () => {
     return <Outlet />;
 };
 
 export const CommonRoutes: RouteObject[] = [
+    { path: '/auth/logout', element: <LogoutPage /> },
     {
         element: <CommonRoute />,
         children: [{ path: '*', element: <NotFound /> }],
