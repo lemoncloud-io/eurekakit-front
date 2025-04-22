13f40c7b760368c18feefc4ee121b9df66188548
luke-lemon <luke@lemoncloud.io>
2025-04-22 14:02:14 +0900

    feat: add useDesktopMobileView and use it

---
+++ apps/web/src/app/hooks/use-desktop-mobile-view/useDesktopMobileView.ts
@@ -0,0 +1,40 @@
+import { useEffect } from 'react';
+
+import { getIsMobile } from '@lemon/shared';
+
+export const useDesktopMobileView = (maxWidth = 400) => {
+    const isMobile = getIsMobile();
+
+    useEffect(() => {
+        const body = document.body;
+        const root = document.getElementById('root');
+
+        if (!body || !root) {
+            return;
+        }
+
+        body.style.display = 'flex';
+        body.style.justifyContent = 'center';
+
+        root.style.position = 'relative';
+        root.style.width = '100%';
+
+        if (!isMobile) {
+            root.style.maxWidth = `${maxWidth}px`;
+            root.style.borderLeftWidth = '1px';
+            root.style.borderRightWidth = '1px';
+
+            const observer = new MutationObserver(() => {
+                document.querySelectorAll('.fixed').forEach(el => {
+                    (el as HTMLElement).style.maxWidth = `${maxWidth}px`;
+                });
+            });
+
+            observer.observe(document.body, { childList: true, subtree: true });
+
+            return () => {
+                observer.disconnect();
+            };
+        }
+    }, [isMobile]);
+};
