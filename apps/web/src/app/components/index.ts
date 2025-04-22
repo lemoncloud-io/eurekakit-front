6ae79c2a53fe4eb79a62a55546aa9d644cf70a4b
luke-lemon <luke@lemoncloud.io>
2025-04-22 15:14:21 +0900

    feat: add ReportModal

--- apps/web/src/app/components/index.ts
+++ apps/web/src/app/components/index.ts
@@ -2,6 +2,8 @@ export * from './image';
 export * from './image-carousel-modal';
 export * from './image-list-viewer';
 
+export * from './report-modal';
+
 export * from './nickname';
 export * from './profile';
 export * from './like-button';
