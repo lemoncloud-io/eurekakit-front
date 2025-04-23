ddad98ed64f0174c2834c4f66f5a3dfd6f88fc8b
louis <louis@lemoncloud.io>
2025-04-23 13:35:40 +0900

    feat: add fixed header and sidebar

--- apps/admin/src/app/shared/components/AdminLayout.tsx
+++ apps/admin/src/app/shared/components/AdminLayout.tsx
@@ -2,16 +2,31 @@ import { Outlet } from 'react-router-dom';
 
 import { cn } from '@lemon/ui-kit';
 
+
 import { Header } from './Header';
 import { Sidebar } from './Sidebar';
+import { useLayoutStore } from '../stores/useLayoutStore';
 
 export const AdminLayout = () => {
+    const isSidebarOpen = useLayoutStore(state => state.isSidebarOpen);
+
     return (
-        <div className="min-h-screen flex flex-col">
-            <Header />
-            <div className="flex flex-1">
-                <Sidebar />
-                <main className={cn('flex-1 p-6 bg-background/95 transition-all duration-300 ml-0')}>
+        <div className="flex min-h-screen flex-col">
+            <div className="fixed left-0 right-0 top-0 z-30">
+                <Header />
+            </div>
+
+            <div className="flex flex-1 pt-16">
+                <div className="fixed left-0 top-16 z-20 h-[calc(100vh-4rem)]">
+                    <Sidebar />
+                </div>
+                <main
+                    className={cn(
+                        'bg-background/95 flex-1 transition-all duration-300',
+                        'h-[calc(100vh-4rem)] w-full overflow-y-auto',
+                        isSidebarOpen ? 'ml-64' : 'ml-0'
+                    )}
+                >
                     <Outlet />
                 </main>
             </div>
