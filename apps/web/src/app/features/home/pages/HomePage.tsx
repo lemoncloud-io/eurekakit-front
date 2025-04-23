ec69cc6e2c90442ed91a3cfd2059cfc7ca3b9036
luke-lemon <luke@lemoncloud.io>
2025-04-23 18:38:10 +0900

    fix: update HomePage layout for better responsiveness

--- apps/web/src/app/features/home/pages/HomePage.tsx
+++ apps/web/src/app/features/home/pages/HomePage.tsx
@@ -4,9 +4,9 @@ import { HomeHeader, HotPostGrid, TotalPostList } from '../components';
 
 export const HomePage = () => {
     return (
-        <div className="flex min-h-full flex-col">
+        <div className="flex min-h-[calc(100vh-64px)] flex-col">
             <HomeHeader />
-            <div className="h-28" />
+            <div className="h-[7.5rem]" />
             <HotPostGrid />
             <div className="w-full px-4">
                 <Separator />
