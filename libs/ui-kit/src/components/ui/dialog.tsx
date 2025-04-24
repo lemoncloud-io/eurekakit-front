ce168f307ee03e11ffd0a8a4163c2c4c36c84775
luke-lemon <luke@lemoncloud.io>
2025-04-24 17:44:20 +0900

    fix: stop propagation of dialog overlay click event

--- libs/ui-kit/src/components/ui/dialog.tsx
+++ libs/ui-kit/src/components/ui/dialog.tsx
@@ -38,7 +38,7 @@ interface DialogContentProps extends React.ComponentPropsWithoutRef<typeof Dialo
 const DialogContent = React.forwardRef<React.ElementRef<typeof DialogPrimitive.Content>, DialogContentProps>(
     ({ className, children, showCloseBtn, ...props }, ref) => (
         <DialogPortal>
-            <DialogOverlay />
+            <DialogOverlay onClick={e => e.preventDefault()} />
             <DialogPrimitive.Content
                 ref={ref}
                 className={cn(
