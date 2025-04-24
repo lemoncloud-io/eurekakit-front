a33a046437ec576065d64d4f94eb01cc8b40d108
luke-lemon <luke@lemoncloud.io>
2025-04-24 16:00:41 +0900

    feat: publish feed page

--- apps/web/src/app/features/feed/pages/feed-page/FeedPage.tsx
+++ apps/web/src/app/features/feed/pages/feed-page/FeedPage.tsx
@@ -1,17 +1,49 @@
-import { Search } from 'lucide-react';
+import { ChevronRight, Search } from 'lucide-react';
 
 import { useFetchInfiniteFeedList } from '@lemon/feeds';
+import { Button } from '@lemon/ui-kit/components/ui/button';
+import { List } from '@lemon/ui-kit/components/ui/list';
 import { Separator } from '@lemon/ui-kit/components/ui/separator';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@lemon/ui-kit/components/ui/tabs';
 
 import { InfiniteList } from '../../../../components';
+import { useNavigate } from '../../../../hooks';
 import { Post } from '../../../post/components';
 import { PostSkeleton } from '../../../post/components/post/PostSkeleton';
+
+export const NoFeedPost = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="flex flex-col items-center justify-center gap-4">
+            <div className="flex flex-col items-center">
+                <span>아직 등록된 게시물이 없습니다.</span>
+                <span>{`다양한 이야기로 다른 사용자들과 소통해 보세요 :)`}</span>
+            </div>
+            <Button
+                className="gap-1 pl-6 pr-4"
+                variant={'secondary'}
+                size={'lg'}
+                onClick={() => navigate('/post/create')}
+            >
+                글 작성하러 가기 <ChevronRight />
+            </Button>
+        </div>
+    );
+};
 
 export const TotalFeedList = () => {
     const { data: feedList, isLoading, hasNextPage, fetchNextPage, isFetchingNextPage } = useFetchInfiniteFeedList();
 
-    return (
+    return isLoading ? (
+        <List seperator={<Separator />}>
+            <PostSkeleton />
+            <PostSkeleton />
+            <PostSkeleton />
+            <PostSkeleton />
+            <PostSkeleton />
+        </List>
+    ) : (
         <InfiniteList
             seperator={<Separator />}
             isFetching={isFetchingNextPage}
@@ -19,17 +51,15 @@ export const TotalFeedList = () => {
             fetchFn={fetchNextPage}
             className="overflow-x-hidden"
         >
-            {isLoading
-                ? Array.from({ length: 5 }).map(() => <PostSkeleton />)
-                : feedList?.list.map(feed => <Post post={feed} />)}
+            {feedList?.list.map(feed => <Post key={feed.id} post={feed} />)}
         </InfiniteList>
     );
 };
 
 export const FeedPage = () => {
     return (
-        <div className="min-h-screen">
-            <header className="flex h-12 items-center px-4">
+        <div className="w-full">
+            <header className="flex h-12 w-full items-center px-4">
                 Feed
                 <button className="ml-auto">
                     <Search size={20} />
@@ -44,6 +74,7 @@ export const FeedPage = () => {
                 <TabsContent value="all" asChild>
                     <TotalFeedList />
                 </TabsContent>
+                <TabsContent value="hot" asChild></TabsContent>
             </Tabs>
         </div>
     );
