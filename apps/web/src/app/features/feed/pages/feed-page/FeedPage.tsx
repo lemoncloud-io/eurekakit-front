9456e35557e3ee9186797ae1fd7a1856078e5193
luke-lemon <luke@lemoncloud.io>
2025-04-24 17:45:38 +0900

    fix: fix feed query cache collision

--- apps/web/src/app/features/feed/pages/feed-page/FeedPage.tsx
+++ apps/web/src/app/features/feed/pages/feed-page/FeedPage.tsx
@@ -2,7 +2,7 @@ import { Search } from 'lucide-react';
 
 import { Images } from '@lemon/assets';
 import { useQueryState } from '@lemon/shared';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@lemon/ui-kit/components/ui/tabs';
+import { Tabs, TabsList, TabsTrigger } from '@lemon/ui-kit/components/ui/tabs';
 
 import { LikedFeedList, TotalFeedList } from '../../components';
 
@@ -12,36 +12,34 @@ export const FeedPage = () => {
     const [feedType, setFeedType] = useQueryState<FeedType>('type', { defaultValue: 'all' });
 
     return (
-        <div className="flex h-full flex-col">
-            <header className="flex h-12 w-full flex-none items-center px-4">
-                <img src={Images.feedPageLogo} />
-                <button className="ml-auto">
-                    <Search size={20} />
-                </button>
-            </header>
-            <Tabs
-                defaultValue={feedType}
-                value={feedType}
-                onValueChange={value => setFeedType(value as FeedType)}
-                className="flex flex-1 flex-col"
-            >
-                <TabsList className="w-full flex-none justify-start border-b">
+        <Tabs
+            defaultValue={feedType}
+            value={feedType}
+            onValueChange={value => setFeedType(value as FeedType)}
+            className="flex h-full flex-1 flex-col"
+        >
+            <header className="bg-background sticky top-0 z-50">
+                <div className="flex h-12 w-full flex-none items-center px-4">
+                    <img src={Images.feedPageLogo} />
+                    <button className="ml-auto">
+                        <Search size={20} />
+                    </button>
+                </div>
+                <TabsList className="sticky top-0 w-full flex-none justify-start border-b">
                     <TabsTrigger value="all">전체</TabsTrigger>
                     <TabsTrigger value="hot">인기글</TabsTrigger>
                     <TabsTrigger value="liked">좋아요</TabsTrigger>
                 </TabsList>
-                <TabsContent value="all" className="flex-1">
-                    <TotalFeedList />
-                </TabsContent>
-                <TabsContent value="hot" className="flex-1">
+            </header>
+            <div className="flex-1">
+                {feedType === 'all' && <TotalFeedList />}
+                {feedType === 'hot' && (
                     <div className="flex h-full items-center justify-center">
                         <span>준비 중입니다.</span>
                     </div>
-                </TabsContent>
-                <TabsContent value="liked" className="flex-1">
-                    <LikedFeedList />
-                </TabsContent>
-            </Tabs>
-        </div>
+                )}
+                {feedType === 'liked' && <LikedFeedList />}
+            </div>
+        </Tabs>
     );
 };
