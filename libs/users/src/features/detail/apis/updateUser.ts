64d69296eded02de8cc0a68c9c9ef07dcc1dd007
luke-lemon <luke@lemoncloud.io>
2025-04-25 16:01:36 +0900

    feat: add updateUser API and useUpdateUser hook for user updates

---
+++ libs/users/src/features/detail/apis/updateUser.ts
@@ -0,0 +1,23 @@
+import { webCore } from '@lemon/web-core';
+
+import { USERS, USER_ENDPOINT } from '../../../consts/apis';
+
+import type { ListResult } from '@lemon/shared';
+import type { UserBody, UserView } from '@lemoncloud/pets-socials-api';
+
+export const updateUser = async (id?: string, body?: UserBody) => {
+    if (!id) {
+        throw new Error('updateUser - @id is required');
+    }
+
+    if (!body) {
+        throw new Error('updateUser - @body is required');
+    }
+
+    const { data } = await webCore
+        .buildSignedRequest({ method: 'PUT', baseURL: [USER_ENDPOINT, USERS, id].join('/') })
+        .setBody({ ...body })
+        .execute<ListResult<UserView>>();
+
+    return data;
+};
