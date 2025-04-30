d9b7c9de9bf4e2e96326a9e488d96fffbaf62aba
luke-lemon <luke@lemoncloud.io>
2025-04-30 15:16:04 +0900

    feat: add profile consts

--- libs/users/src/consts/apis.ts
+++ libs/users/src/consts/apis.ts
@@ -2,4 +2,5 @@ export const USER_ENDPOINT = import.meta.env.VITE_OAUTH_ENDPOINT;
 export const PET_ENDPOINT = import.meta.env.VITE_PET_API_ENDPOINT?.toLowerCase();
 
 export const USERS = 'users';
-export const FEEDS = 'feeds';
+
+export const SOCIAL = 'social';
