29bb7ab1dd095b78ac64648a7dc5a968c3ab5079
luke-lemon <luke@lemoncloud.io>
2025-05-02 13:44:08 +0900

    feat: add attachUser$ToListResult func and ViewWithUser$

---
+++ libs/feeds/src/features/list/utils/attachUser$ToListResult.ts
@@ -0,0 +1,18 @@
+import type { ViewWithUser$ } from '../types';
+import type { ListResult } from '@lemon/shared';
+import type { UserView } from '@lemon/users';
+
+export const attachUser$ToListResult = <L extends ListResult<V>, V extends ViewWithUser$>(
+    listResult: L,
+    userList: UserView[]
+) => {
+    const withUserList = listResult.list.map(data => {
+        const ownerId = data.user$.id;
+
+        const owner = userList.find(user => user.id === ownerId);
+
+        return { ...data, user$: owner };
+    });
+
+    return { ...listResult, list: withUserList };
+};
