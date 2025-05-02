5c32fa7f9eabedd46dff281ffb4ed8e3a7bfc0f1
luke-lemon <luke@lemoncloud.io>
2025-05-02 13:49:59 +0900

    refactor: refactor with util func

--- libs/comments/src/features/list/quries/useFetchInfiniteFeedCommentList.ts
+++ libs/comments/src/features/list/quries/useFetchInfiniteFeedCommentList.ts
@@ -1,53 +1,23 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
 
+import { flattenInfiniteListResult, getListResultNextPage } from '@lemon/shared';
+
 import { commentKeys } from '../../../consts';
 import { fetchFeedCommentList } from '../apis';
+import { attachUser$ToListResult } from '../utils';
 
-import type { CommentListParams, CommentView } from '../../../types';
-import type { ListResult } from '@lemon/shared';
-import type { UserView } from '@lemoncloud/pets-socials-api';
+import type { CommentListParams } from '../../../types';
 
 export const useFetchInfiniteFeedCommentList = (feedId?: string, params?: CommentListParams) =>
     useInfiniteQuery({
         queryKey: commentKeys.list({ feedId }, true),
         queryFn: ({ pageParam = 0 }) => fetchFeedCommentList(feedId, { ...params, page: pageParam }),
         initialPageParam: 0,
-        getNextPageParam: lastPage => {
-            const { page, total, limit } = lastPage;
-
-            if (page == null || total == null || limit == null) {
-                return undefined;
-            }
-
-            const maxPages = Math.ceil(total / limit);
-
-            return page + 1 < maxPages ? page + 1 : undefined;
-        },
+        getNextPageParam: getListResultNextPage,
         select: data => {
-            const listResult = data.pages.reduce(
-                (acc, cur) => {
-                    return { ...acc, list: [...acc.list, ...cur.list] };
-                },
-                { list: [], total: data.pages[0].total } as ListResult<CommentView>
-            );
+            const flattenedListResult = flattenInfiniteListResult(data);
+            const commentListWithUser = attachUser$ToListResult(flattenedListResult, flattenedListResult.Users);
 
-            const userInfo = data.pages.reduce((acc, cur) => {
-                cur.Users.forEach(user => {
-                    if (acc.has(user.id)) {
-                        return acc;
-                    }
-
-                    acc.set(user.id, user);
-
-                    return acc;
-                });
-                return acc;
-            }, new Map<string, UserView>());
-
-            const commentListWithUser = listResult.list.map(feed => ({ ...feed, user$: userInfo.get(feed.userId) }));
-
-            console.log(commentListWithUser);
-
-            return { ...listResult, list: commentListWithUser };
+            return { ...flattenedListResult, list: commentListWithUser };
         },
     });
