c7f2c2a23f8a41fad7128e2e39e6d95d8adc185e
luke-lemon <luke@lemoncloud.io>
2025-05-08 18:38:55 +0900

    feat: add InfiniteFetchedList

--- apps/web/src/app/components/infinite-list/InfiniteList.tsx
+++ apps/web/src/app/components/infinite-list/InfiniteList.tsx
@@ -5,13 +5,12 @@ import { Loader2 } from 'lucide-react';
 import { useIntersectionObserver } from '@lemon/shared';
 import { List } from '@lemon/ui-kit/components/ui/list';
 
-interface InfiniteListProps extends ComponentProps<typeof List> {
+export interface InfiniteListProps extends ComponentProps<typeof List> {
     isFetching: boolean;
     showTrigger: boolean;
     fetchFn: (...params: any) => Promise<any>;
 }
 
-// TODO : 에러 시 표시 및 refetch 버튼 추가
 export const InfiniteList = ({ children, isFetching, showTrigger, fetchFn, ...listProps }: InfiniteListProps) => {
     const { setRef, isIntersecting } = useIntersectionObserver();
 
