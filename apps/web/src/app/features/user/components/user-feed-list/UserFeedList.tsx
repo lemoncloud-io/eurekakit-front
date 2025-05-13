151a6881355ca67e5938851b802190347d110d78
luke-lemon <luke@lemoncloud.io>
2025-05-13 18:39:06 +0900

    refactor: change user related feed list component to InfiniteFetchedList

--- apps/web/src/app/features/user/components/user-feed-list/UserFeedList.tsx
+++ apps/web/src/app/features/user/components/user-feed-list/UserFeedList.tsx
@@ -6,62 +6,61 @@ import { List } from '@lemon/ui-kit/components/ui/list';
 import { Separator } from '@lemon/ui-kit/components/ui/separator';
 import { useFetchProfile } from '@lemon/users';
 
-import { InfiniteList, Link } from '../../../../components';
+import { ErrorWithRetry, Link } from '../../../../components';
+import { InfiniteFetchedList } from '../../../../components/infinite-fetched-list/InfiniteFetchedList';
+import { withQueryErrorBoundary, withSuspense } from '../../../../utils';
 import { Feed, FeedSkeleton } from '../../../feed/components';
 
-export const UserFeedList = () => {
-    const { data: profile, isLoading: isProfileLoading } = useFetchProfile();
+const UserFeedListSkeleton = () => (
+    <List seperator={<Separator />}>
+        {Array.from({ length: window.innerHeight / 120 - 2 }).map(() => (
+            <FeedSkeleton />
+        ))}
+    </List>
+);
+
+const UserFeedListEmptyFallback = () => (
+    <div className="text-secondary-foreground flex flex-1 flex-col items-center justify-center gap-3 text-sm">
+        <div className="flex flex-col items-center justify-center">
+            <span>글 작성을 아직 안하셨군요!</span>
+            <span>다양한 이야기로 다른 사용자들과 소통해 보세요!</span>
+        </div>
+        <Button variant={'secondary'} className="gap-1 pl-8 pr-6" size={'lg'}>
+            글 작성하러 가기
+            <ChevronRight />
+        </Button>
+    </div>
+);
+
+const UserFeedListContent = () => {
+    const { data: profile } = useFetchProfile();
 
     const {
         data: feedList,
         hasNextPage,
-        isFetching,
+        isFetchingNextPage,
         fetchNextPage,
-        isLoading,
     } = useFetchInfiniteUserFeedList(profile?.id);
 
-    const isEmptyList = feedList?.total === 0;
-
-    if (isLoading || isProfileLoading) {
-        return (
-            <List seperator={<Separator />}>
-                {Array.from({ length: window.innerHeight / 120 - 2 }).map(() => (
-                    <FeedSkeleton />
-                ))}
-            </List>
-        );
-    }
-
-    if (isEmptyList) {
-        return (
-            <div className="text-secondary-foreground flex flex-1 flex-col items-center justify-center gap-3 text-sm">
-                <div className="flex flex-col items-center justify-center">
-                    <span>글 작성을 아직 안하셨군요!</span>
-                    <span>다양한 이야기로 다른 사용자들과 소통해 보세요!</span>
-                </div>
-                <Button variant={'secondary'} className="gap-1 pl-8 pr-6" size={'lg'}>
-                    글 작성하러 가기
-                    <ChevronRight />
-                </Button>
-            </div>
-        );
-    }
-
     return (
-        <InfiniteList
-            showTrigger={hasNextPage}
-            isFetching={isFetching}
-            fetchFn={fetchNextPage}
+        <InfiniteFetchedList
+            items={feedList?.list}
+            renderItem={feed => (
+                <Link to={`/feed/${feed.id}`} key={feed.id}>
+                    <Feed feed={feed} />
+                </Link>
+            )}
             seperator={<Separator />}
+            fetchFn={fetchNextPage}
+            isFetching={isFetchingNextPage}
+            showTrigger={hasNextPage}
+            emptyFallBack={<UserFeedListEmptyFallback />}
             className="overflow-x-hidden"
-        >
-            {feedList?.list
-                .map(feed => ({ ...feed, user$: profile! }))
-                .map(feed => (
-                    <Link to={`/feed/${feed.id}`} key={feed.id}>
-                        <Feed feed={feed} />
-                    </Link>
-                ))}
-        </InfiniteList>
+        />
     );
 };
+
+export const UserFeedList = withQueryErrorBoundary(
+    withSuspense(UserFeedListContent, <UserFeedListSkeleton />),
+    errorProps => <ErrorWithRetry {...errorProps} className="h-full" />
+);
