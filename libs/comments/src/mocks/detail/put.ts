6d8e2c90af340023c9f0a619f758923ceb5d3e80
luke-lemon <luke@lemoncloud.io>
2025-05-16 15:24:03 +0900

    feat: add handler for hiding comments and updating feed visibility

--- libs/comments/src/mocks/detail/put.ts
+++ libs/comments/src/mocks/detail/put.ts
@@ -2,7 +2,7 @@ import { HttpResponse, http } from 'msw';
 
 import { db } from '@lemon/mock-db';
 
-import { COMMENTS, CONTENT_ENDPOINT } from '../../consts';
+import { COMMENTS, CONTENT_ENDPOINT, HIDE } from '../../consts';
 
 export const putHandler = [
     http.put([CONTENT_ENDPOINT, COMMENTS, ':commentId'].join('/'), async ({ request, params }) => {
@@ -17,6 +17,27 @@ export const putHandler = [
         const comment = db.comment.update({
             where: { id: { equals: commentId } },
             data: { ...body, updateAt: Date.now() },
+        });
+
+        return HttpResponse.json(comment);
+    }),
+    http.put([CONTENT_ENDPOINT, COMMENTS, ':commentId', HIDE].join('/'), ({ params }) => {
+        const commentId = params['commentId'] as string | undefined;
+
+        if (!commentId) {
+            return HttpResponse.json({ message: 'Invalid commentId' }, { status: 400 });
+        }
+
+        const comment = db.comment.update({
+            where: { id: { equals: commentId } },
+            data: { hidden: true, updateAt: Date.now() },
+        });
+
+        const feed = db.feed.findFirst({ where: { id: { equals: comment?.feedId } } });
+
+        db.feed.update({
+            where: { id: { equals: comment?.feedId } },
+            data: { commentHidden: (feed?.commentHidden ?? 0) + 1 },
         });
 
         return HttpResponse.json(comment);
