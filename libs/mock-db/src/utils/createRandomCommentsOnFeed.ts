fbb2f2e19fc8e4c368b92415f5eb3aea59512f12
luke-lemon <luke@lemoncloud.io>
2025-05-16 14:36:09 +0900

    feat: add likeCount to random comments and feeds generation

--- libs/mock-db/src/utils/createRandomCommentsOnFeed.ts
+++ libs/mock-db/src/utils/createRandomCommentsOnFeed.ts
@@ -6,9 +6,12 @@ import { createRandomImages } from './createRandomImages';
 export const createRandomCommentsOnFeed = (feedId: string, count: number) =>
     Array.from({ length: count }).map((_, idx) => {
         const userId = db.user.create({ id: faker.string.uuid() }).id;
+        const likeCount = Math.floor(Math.random() * (10 ** Math.random() * 10));
+
         return db.comment.create({
             id: `${feedId}:${idx}`,
             userId,
+            likeCount,
             image$$: createRandomImages(Math.floor(Math.random() * 6)),
             feedId,
         });
--- libs/mock-db/src/utils/createRandomFeeds.ts
+++ libs/mock-db/src/utils/createRandomFeeds.ts
@@ -6,6 +6,7 @@ import { createRandomImages } from './createRandomImages';
 export const createRandomFeeds = (userId: string, count: number) =>
     Array.from({ length: count }, () => {
         const images = createRandomImages(Math.floor(Math.random() * 6));
+        const likeCount = Math.floor(Math.random() * (10 ** Math.random() * 10000));
 
-        return db.feed.create({ id: faker.string.uuid(), image$$: images, userId });
+        return db.feed.create({ id: faker.string.uuid(), image$$: images, userId, likeCount });
     });
