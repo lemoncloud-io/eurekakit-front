1512197a2ca5942c2416b0c2b7e524974c0a6b66
luke-lemon <luke@lemoncloud.io>
2025-05-16 13:04:47 +0900

    fix: fix post feed mock handler to use my id

--- libs/feeds/src/mocks/detail/post.ts
+++ libs/feeds/src/mocks/detail/post.ts
@@ -1,7 +1,7 @@
 import { faker } from '@faker-js/faker';
 import { HttpResponse, delay, http } from 'msw';
 
-import { db } from '@lemon/mock-db';
+import { db, myId } from '@lemon/mock-db';
 
 import { CONTENT_ENDPOINT, FEED, USERS } from '../../consts';
 
@@ -9,13 +9,15 @@ export const postHandler = [
     http.post([CONTENT_ENDPOINT, USERS, '0', FEED].join('/'), async ({ request }) => {
         const body = await request.json();
 
+        const my = db.user.findFirst({ where: { id: { equals: myId } } });
+
         const newFeed = db.feed.create({
             ...body,
             id: faker.string.uuid(),
             createdAt: Date.now(),
             likeCount: 0,
             viewCount: 0,
-            user$: { id: faker.string.uuid(), nick: 'DefaultUser' }, // 필요 시 추후 수정
+            user$: my,
             hidden: false,
         });
 
