dd23de7cba33dbe89021cf7022386150e342f95f
luke-lemon <luke@lemoncloud.io>
2025-05-16 16:10:40 +0900

    feat: include upload handler in mock service worker setup

--- apps/web/src/app/mocks/browser.ts
+++ apps/web/src/app/mocks/browser.ts
@@ -2,6 +2,7 @@ import { setupWorker } from 'msw/browser';
 
 import { commentsHandler } from '@lemon/comments';
 import { feedsHandler } from '@lemon/feeds';
+import { uploadHandler } from '@lemon/uploads';
 import { usersHandler } from '@lemon/users';
 
-export const worker = setupWorker(...feedsHandler, ...commentsHandler, ...usersHandler);
+export const worker = setupWorker(...feedsHandler, ...commentsHandler, ...usersHandler, ...uploadHandler);
