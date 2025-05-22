e01050d72b6b2cef46b7f4fd219baeb55387123c
luke-lemon <luke@lemoncloud.io>
2025-05-22 17:38:08 +0900

    chore: remove RedirectByAuth Component

--- apps/web/src/app/features/auth/components/index.ts
+++ apps/web/src/app/features/auth/components/index.ts
@@ -1,3 +1,2 @@
 export * from './auth-guard';
 export * from './owner-guard';
-export * from './redirect-by-auth';
--- apps/web/src/app/features/auth/components/redirect-by-auth/RedirectByAuth.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-import { useWebCoreStore } from '@lemon/web-core';
-
-export const RedirectByAuth = () => {
-    const { isAuthenticated } = useWebCoreStore();
-
-    return isAuthenticated ? <Navigate to="/" /> : <Navigate to="/auth" />;
-};
