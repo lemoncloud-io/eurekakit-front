89246e7004acabc8a27eda03c569807ce120e82b
Luke <luke@lemoncloud.io>
2025-05-23 13:03:29 +0900

    [웹] Forbidden 403 페이지 추가 (#49)
    * feat: add Forbidden403 and change ErrorPage to NotFound404
    
    * feat: throw error when 403
    
    * fix: change ErrorPage to NotFound404
    
    * feat: add Forbidden 403

--- apps/web/src/app/pages/Forbidden403.tsx
+++ apps/web/src/app/pages/Forbidden403.tsx
@@ -4,24 +4,25 @@ import { useWebCoreStore } from '@lemon/web-core';
 
 import { Link } from '../components';
 
-interface ErrorPageProps {
-    title?: string;
-    description?: string;
-    sorryMsg?: string;
-}
+import type { AxiosError } from 'axios';
+import type { FallbackProps } from 'react-error-boundary';
 
-export const ErrorPage = ({ title, description, sorryMsg }: ErrorPageProps) => {
+export const Forbidden403 = ({ error }: FallbackProps) => {
     const isAuthenticated = useWebCoreStore(store => store.isAuthenticated);
+
+    if ((error as AxiosError).isAxiosError && (error as AxiosError).response?.status !== 403) {
+        throw error;
+    }
 
     return (
         <div className="flex h-full w-full flex-col items-center justify-center gap-4 p-6">
             <CircleAlert className="text-muted-foreground" size={32} />
             <div className="flex flex-col items-center gap-1 font-bold">
-                <span className="text-2xl">{title || 'Sorry,'}</span>
-                <span>{description}</span>
+                <span className="text-2xl">Sorry,</span>
+                <span>You do not gave permission to accesss this page.</span>
             </div>
             <span className="text-muted-foreground text-balance text-center text-sm">
-                {sorryMsg || 'We will resolve the issue as soon as possible to ensure smooth service'}
+                We will resolve the issue as soon as possible to ensure smooth service
             </span>
             <br />
             <Link to={isAuthenticated ? '/' : '/auth'} className="text-accent-foreground flex items-center gap-1">
