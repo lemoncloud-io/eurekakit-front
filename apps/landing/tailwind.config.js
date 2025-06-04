552342eeae70dbf738e602c9a3bb274eb701e88f
luke-lemon <luke@lemoncloud.io>
2025-06-04 15:37:45 +0900

    feat: setup tailwind

--- apps/landing/tailwind.config.js
+++ apps/landing/tailwind.config.js
@@ -2,14 +2,13 @@ const { join } = require('path');
 
 const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
 
+const TailwindConfig = require('../../libs/ui-kit/tailwind.config');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    ...TailwindConfig,
     content: [
         join(__dirname, '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'),
         ...createGlobPatternsForDependencies(__dirname),
     ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
 };
