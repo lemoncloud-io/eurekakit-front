a6b5e6ceacf86e0056ca1e6e0f4c0250d5e847b4
louis <louis@lemoncloud.io>
2025-06-11 11:32:45 +0900

    feat: deploy landing page to production

--- apps/landing/vite.config.ts
+++ apps/landing/vite.config.ts
@@ -2,30 +2,70 @@
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, searchForWorkspaceRoot } from 'vite';
 
 export default defineConfig(() => ({
     root: __dirname,
     cacheDir: '../../node_modules/.vite/apps/landing',
+
+    define: {
+        'process.env': {},
+        'process.env.I18N_VERSION': JSON.stringify(Date.now().toString()),
+        ...(process.env.NODE_ENV === 'development'
+            ? {
+                  global: 'window',
+                  'process.env.I18N_VERSION': JSON.stringify('dev'),
+              }
+            : {}),
+    },
+
+    resolve: {
+        alias: {
+            '@lemon/assets': '/assets/src/index.ts',
+            ...(process.env.NODE_ENV !== 'development'
+                ? {
+                      './runtimeConfig': './runtimeConfig.browser', // fix production build
+                  }
+                : {}),
+        },
+    },
+
     server: {
         port: 4200,
         host: 'localhost',
+        fs: {
+            allow: [searchForWorkspaceRoot(process.cwd()), searchForWorkspaceRoot(process.cwd()) + '../../../assets'],
+        },
     },
+
     preview: {
         port: 4300,
         host: 'localhost',
     },
+
     plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+
     // Uncomment this if you are using workers.
     // worker: {
     //  plugins: [ nxViteTsPaths() ],
     // },
     build: {
+        sourcemap: process.env.VITE_ENV !== 'PROD',
+        minify: 'terser',
         outDir: '../../dist/apps/landing',
-        emptyOutDir: true,
         reportCompressedSize: true,
         commonjsOptions: {
+            include: [/node_modules/],
+            extensions: ['.js', '.cjs'],
+            strictRequires: true,
+            // https://stackoverflow.com/questions/62770883/how-to-include-both-import-and-require-statements-in-the-bundle-using-rollup
             transformMixedEsModules: true,
+        },
+    },
+
+    css: {
+        modules: {
+            localsConvention: 'camelCase',
         },
     },
 }));
